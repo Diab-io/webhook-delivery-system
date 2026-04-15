731ddbf19042a8a8368c6c36a6ba786cb3c22fb4
David Odimayo <odimayodavid7@gmail.com>
2026-04-15 11:05:09 +0100

    fix: ommision in calling encode function

--- app/clients/dependencies.py
+++ app/clients/dependencies.py
@@ -1,7 +1,7 @@
 import bcrypt
 
 def hash_api_key(api_key: str) -> str:
-        return bcrypt.hashpw(api_key.encode, bcrypt.gensalt()).decode()
+        return bcrypt.hashpw(api_key.encode(), bcrypt.gensalt()).decode()
     
 def verify_api_key(plain_key: str, hashed_key: str) -> bool:
     return bcrypt.checkpw(plain_key.encode(), hashed_key.encode())
