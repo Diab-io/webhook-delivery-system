b5bece669c32a8b70e8818b7c58c18a15be2b52c
David Odimayo <odimayodavid7@gmail.com>
2026-04-15 08:52:41 +0100

    feat: Added apps table

---
+++ app/apps/models.py
@@ -0,0 +1,19 @@
+from uuid import uuid4, UUID
+from app.core.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, UniqueConstraint
+from datetime import datetime, timezone
+
+class App(Base):
+    __tablename__ = 'apps'
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    name: Mapped[str]
+    api_key: Mapped[str] = mapped_column(unique=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"))
+    active: Mapped[bool] = mapped_column(default=True)
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
+
+    __table_args__ = (
+        UniqueConstraint('name', 'user_id', name='uq_app_name_user'),
+    )
