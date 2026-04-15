807849e2c905c062864723f995138e686a2fd36c
David Odimayo <odimayodavid7@gmail.com>
2026-04-15 11:03:31 +0100

    feat: migrations files updated for app model inclusion

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 from app.core.database import Base
 from app.users.models import User
+from app.clients.models import App
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
