87dd9f5d7a99fe78941218ea2802cd2d48d84f58
David Odimayo <odimayodavid7@gmail.com>
2026-04-15 11:02:50 +0100

    fix: Changed table name to users as it was conflicting with postgres keyword

--- app/users/models.py
+++ app/users/models.py
@@ -3,4 +3,5 @@ from app.core.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
+    __tablename__ = "users"
     username: Mapped[str] = mapped_column(nullable=False, unique=True, index=True)
