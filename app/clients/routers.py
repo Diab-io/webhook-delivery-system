b6475d0f259d2b085cb737061d8a5e62a01425c6
David Odimayo <odimayodavid7@gmail.com>
2026-04-16 19:23:14 +0100

    feat: Added route to get webhooks related to a specific app

--- app/clients/routers.py
+++ app/clients/routers.py
@@ -2,6 +2,7 @@ from app.clients.schemas import AppCreateRequest, AppCreateResponse, AppRead
 from app.users.service import current_active_user
 from app.users.models import User
 from app.clients.services import get_app_service, AppService
+from app.webhooks.schemas import WebhookResponse
 from fastapi import APIRouter, Depends
 from typing import List
 from uuid import UUID
@@ -40,4 +41,11 @@ async def deactivate_app(
 ):
     return await service.deactivate_app(current_user, id)
 
+@router.get("/{id}/webhooks", response_model=List[WebhookResponse])
+async def get_app_webhooks(
+    id: UUID,
+    current_user: User = Depends(current_active_user),
+    service: AppService = Depends(get_app_service)
+):
+    return await service.get_app_webhooks(current_user, id)
 
