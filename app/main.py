3968d76956c18f4ac8da3833e2c9ed0a90953b84
David Odimayo <odimayodavid7@gmail.com>
2026-04-17 17:05:30 +0100

    chore: Hooked webhooks router to main app

--- app/main.py
+++ app/main.py
@@ -5,38 +5,44 @@ from app.users.schemas import UserCreate, UserRead, UserUpdate
 from app.users.service import auth_backend, current_active_user, fastapi_users
 from app.core.exceptions import register_exception_handlers
 from app.clients.routers import router as client_route
+from app.webhooks.routers import router as webhook_route
 
 
 app = FastAPI()
 register_exception_handlers(app)
 
 app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/api/auth/jwt", tags=["auth"]
+    fastapi_users.get_auth_router(auth_backend), prefix="/api/auth/jwt", tags=["Auth"]
 )
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/api/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 app.include_router(
     fastapi_users.get_reset_password_router(),
     prefix="/api/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 app.include_router(
     fastapi_users.get_verify_router(UserRead),
     prefix="/api/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 app.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/api/users",
-    tags=["users"],
+    tags=["Users"],
 )
 app.include_router(
     client_route,
     prefix="/api/apps",
     tags=["Apps"],
+)
+app.include_router(
+    webhook_route,
+    prefix="/api/webhooks",
+    tags=["Webhooks"],
 )
 
 
